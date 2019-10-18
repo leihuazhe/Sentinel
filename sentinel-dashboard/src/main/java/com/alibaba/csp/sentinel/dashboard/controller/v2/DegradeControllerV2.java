0c75b469fd942c633448035a2785dab30033d32c
有事没事 <wangchongya@163.com>
2019-10-18 17:27:59 +0800

    degrade bug fix

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/DegradeControllerV2.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/DegradeControllerV2.java
@@ -75,12 +75,12 @@ public class DegradeControllerV2 {
         if (StringUtil.isEmpty(app)) {
             return Result.ofFail(-1, "app can't be null or empty");
         }
-        if (StringUtil.isEmpty(ip)) {
-            return Result.ofFail(-1, "ip can't be null or empty");
-        }
-        if (port == null) {
-            return Result.ofFail(-1, "port can't be null");
-        }
+//        if (StringUtil.isEmpty(ip)) {
+//            return Result.ofFail(-1, "ip can't be null or empty");
+//        }
+//        if (port == null) {
+//            return Result.ofFail(-1, "port can't be null");
+//        }
         try {
             List<DegradeRuleEntity> rules = ruleProvider.getRules(app);
             rules = repository.saveAll(rules);
@@ -122,8 +122,8 @@ public class DegradeControllerV2 {
         }
         DegradeRuleEntity entity = new DegradeRuleEntity();
         entity.setApp(app.trim());
-        entity.setIp(ip.trim());
-        entity.setPort(port);
+//        entity.setIp(ip.trim());
+//        entity.setPort(port);
         entity.setLimitApp(limitApp.trim());
         entity.setResource(resource.trim());
         entity.setCount(count);
