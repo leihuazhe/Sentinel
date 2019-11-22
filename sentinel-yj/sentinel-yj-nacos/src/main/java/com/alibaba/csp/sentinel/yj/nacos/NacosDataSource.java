ffb0b5e915022d749261a5ecd2058dfa37ef9017
有事没事 <wangchongya@163.com>
2019-11-22 18:34:17 +0800

    sentinel cluster nacos

--- sentinel-yj/sentinel-yj-nacos/src/main/java/com/alibaba/csp/sentinel/yj/nacos/NacosDataSource.java
+++ sentinel-yj/sentinel-yj-nacos/src/main/java/com/alibaba/csp/sentinel/yj/nacos/NacosDataSource.java
@@ -1,8 +1,17 @@
 package com.alibaba.csp.sentinel.yj.nacos;
 
+import com.alibaba.csp.sentinel.cluster.ClusterStateManager;
+import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientAssignConfig;
+import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientConfig;
+import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientConfigManager;
+import com.alibaba.csp.sentinel.cluster.flow.rule.ClusterFlowRuleManager;
+import com.alibaba.csp.sentinel.cluster.flow.rule.ClusterParamFlowRuleManager;
+import com.alibaba.csp.sentinel.cluster.server.config.ClusterServerConfigManager;
+import com.alibaba.csp.sentinel.cluster.server.config.ServerTransportConfig;
 import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
 import com.alibaba.csp.sentinel.log.RecordLog;
+import com.alibaba.csp.sentinel.property.SentinelProperty;
 import com.alibaba.csp.sentinel.slots.block.authority.AuthorityRule;
 import com.alibaba.csp.sentinel.slots.block.authority.AuthorityRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
@@ -11,7 +20,10 @@ import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRuleManager;
 import com.alibaba.csp.sentinel.slots.system.SystemRule;
 import com.alibaba.csp.sentinel.slots.system.SystemRuleManager;
+import com.alibaba.csp.sentinel.transport.config.TransportConfig;
 import com.alibaba.csp.sentinel.util.AppNameUtil;
+import com.alibaba.csp.sentinel.util.HostNameUtil;
+import com.alibaba.csp.sentinel.util.function.Function;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.alibaba.nacos.api.NacosFactory;
@@ -28,19 +40,20 @@ import java.util.Properties;
 public class NacosDataSource {
 
 
-
     public static final String GROUP_ID = "SENTINEL_GROUP";
 
     public static final String FLOW_DATA_ID_POSTFIX = "-flow-rules";
     public static final String SYSTEM_DATA_ID_POSTFIX = "-system-rules";
     public static final String AUTHORITY_DATA_ID_POSTFIX = "-authority-rules";
     public static final String PARAM_FLOW_DATA_ID_POSTFIX = "-param-rules";
-    public static final String CLUSTER_MAP_DATA_ID_POSTFIX = "-cluster-map";
+
 
     /**
      * cc for `cluster-client`
      */
-    public static final String CLIENT_CONFIG_DATA_ID_POSTFIX = "-cc-config";
+    public static final String CLUSTER_MAP_DATA_ID_POSTFIX = "-cluster-map";
+    public static final String CLIENT_CONFIG_POSTFIX = "-cluster-client-config";
+
     /**
      * cs for `cluster-server`
      */
@@ -50,6 +63,7 @@ public class NacosDataSource {
 
 
     private NacosConfig nacosConfig;
+    private NacosConfig nacosClusterConfig;
     private ConfigService configService = null;
     private Properties properties = new Properties();
 
@@ -66,19 +80,45 @@ public class NacosDataSource {
             this.configService = NacosFactory.createConfigService(this.properties);
         }catch (Exception e){
             RecordLog.warn("[NacosDataSource] Error occurred when initializing Nacos data source", e);
-            e.printStackTrace();
         }
 
-        loadMyNamespaceRules();
+        // Register client dynamic rule data source.
+        initDynamicRuleProperty(AppNameUtil.getAppName(),this.nacosConfig);
+
+
+        if(nacosClusterConfig!=null){
+            /**client cluster*/
+            // Register token client related data source.
+            // Token client common config:
+            initClientConfigProperty(AppNameUtil.getAppName(),this.nacosClusterConfig);
+            // Token client assign config (e.g. target token server) retrieved from assign map:
+            initClientServerAssignProperty(AppNameUtil.getAppName(),this.nacosClusterConfig);
+
+            /** cluster */
+            // Register token server related data source.
+            // Register dynamic rule data source supplier for token server:
+            registerClusterRuleSupplier(AppNameUtil.getAppName(),this.nacosConfig);
+            // Token server transport config extracted from assign map:
+            initServerTransportConfigProperty(AppNameUtil.getAppName(),this.nacosClusterConfig);
+
+            // Init cluster state property for extracting mode from cluster map data source.
+            initStateProperty(AppNameUtil.getAppName(),this.nacosClusterConfig);
+        }
+
     }
 
-    private void loadMyNamespaceRules() {
 
-        String flowDataId = AppNameUtil.getAppName() + FLOW_DATA_ID_POSTFIX ;
-        String systemDataId = AppNameUtil.getAppName() + SYSTEM_DATA_ID_POSTFIX ;
-        String authorityDataId = AppNameUtil.getAppName() + AUTHORITY_DATA_ID_POSTFIX ;
-        String paramDataId = AppNameUtil.getAppName() + PARAM_FLOW_DATA_ID_POSTFIX ;
-        String clusterDataId = AppNameUtil.getAppName() + CLUSTER_MAP_DATA_ID_POSTFIX ;
+    /**
+     * 动态流控
+     * @param appName
+     * @param nacosConfig
+     */
+    private void initDynamicRuleProperty(String appName,final NacosConfig nacosConfig) {
+
+        String flowDataId = appName + FLOW_DATA_ID_POSTFIX ;
+        String systemDataId = appName + SYSTEM_DATA_ID_POSTFIX ;
+        String authorityDataId = appName + AUTHORITY_DATA_ID_POSTFIX ;
+        String paramDataId = appName + PARAM_FLOW_DATA_ID_POSTFIX ;
 
         Properties properties = new Properties();
         properties.put(PropertyKeyConst.SERVER_ADDR, nacosConfig.getRemoteAddress());
@@ -147,11 +187,194 @@ public class NacosDataSource {
 
     }
 
+
+    /**
+     * 集群下Client配置
+     * @param appName
+     * @param nacosConfig
+     */
+    private void initClientConfigProperty(String appName,final NacosConfig nacosConfig) {
+        String configDataId = appName + CLIENT_CONFIG_POSTFIX;
+        ReadableDataSource<String, ClusterClientConfig> clientConfigDs = new CustomNacosDataSource<>(configService, properties, nacosConfig.getGroupId(),
+                configDataId, new Converter<String, ClusterClientConfig>() {
+            @Override
+            public ClusterClientConfig convert(String source) {
+                return JSON.parseObject(source, new TypeReference<ClusterClientConfig>() {});
+            }
+        });
+        ClusterClientConfigManager.registerClientConfigProperty(clientConfigDs.getProperty());
+    }
+
+
+    /**
+     * 集群服务配置
+     * @param appName
+     * @param nacosConfig
+     */
+    private void initServerTransportConfigProperty(String appName,NacosConfig nacosConfig) {
+        String clusterMapDataId = appName + CLUSTER_MAP_DATA_ID_POSTFIX;
+        ReadableDataSource<String, ServerTransportConfig> serverTransportDs = new CustomNacosDataSource<>(configService, properties, nacosConfig.getGroupId(),
+                clusterMapDataId, new Converter<String, ServerTransportConfig>() {
+            @Override
+            public ServerTransportConfig convert(String source) {
+                List<ClusterGroupEntity> groupList = JSON.parseObject(source, new TypeReference<List<ClusterGroupEntity>>() {});
+                if(groupList!=null){
+                    return extractServerTransportConfig(groupList);
+                }
+                return null;
+            }
+        });
+
+        ClusterServerConfigManager.registerServerTransportProperty(serverTransportDs.getProperty());
+    }
+
+
+
+    private void registerClusterRuleSupplier(String appName,final NacosConfig nacosConfig) {
+        // Register cluster flow rule property supplier which creates data source by namespace.
+        // Flow rule dataId format: ${namespace}-flow-rules
+        ClusterFlowRuleManager.setPropertySupplier(new Function<String, SentinelProperty<List<FlowRule>>>() {
+            @Override
+            public SentinelProperty<List<FlowRule>> apply(String namespace) {
+                ReadableDataSource<String, List<FlowRule>> ds = new CustomNacosDataSource<>(configService, properties, nacosConfig.getGroupId(),
+                        namespace + FLOW_DATA_ID_POSTFIX, new Converter<String, List<FlowRule>>() {
+                    @Override
+                    public List<FlowRule> convert(String source) {
+                        return JSON.parseObject(source, new TypeReference<List<FlowRule>>() {});
+                    }
+                });
+                return ds.getProperty();
+            }
+        });
+
+        // Register cluster parameter flow rule property supplier which creates data source by namespace.
+        ClusterParamFlowRuleManager.setPropertySupplier(new Function<String, SentinelProperty<List<ParamFlowRule>>>() {
+            @Override
+            public SentinelProperty<List<ParamFlowRule>> apply(String namespace) {
+                ReadableDataSource<String, List<ParamFlowRule>> ds = new CustomNacosDataSource<>(configService, properties, nacosConfig.getGroupId(),
+                        namespace + PARAM_FLOW_DATA_ID_POSTFIX, new Converter<String, List<ParamFlowRule>>() {
+                    @Override
+                    public List<ParamFlowRule> convert(String source) {
+                        return JSON.parseObject(source, new TypeReference<List<ParamFlowRule>>() {});
+                    }
+                });
+                return ds.getProperty();
+            }
+        });
+    }
+
+
+    private void initClientServerAssignProperty(String appName,final NacosConfig nacosConfig) {
+        // Cluster map format:
+        // [{"clientSet":["112.12.88.66@8729","112.12.88.67@8727"],"ip":"112.12.88.68","machineId":"112.12.88.68@8728","port":11111}]
+        // machineId: <ip@commandPort>, commandPort for port exposed to Sentinel dashboard (transport module)
+        String clusterMapDataId = appName + CLUSTER_MAP_DATA_ID_POSTFIX;
+        ReadableDataSource<String, ClusterClientAssignConfig> clientAssignDs = new CustomNacosDataSource<>(configService, properties, nacosConfig.getGroupId(),
+                clusterMapDataId, new Converter<String, ClusterClientAssignConfig>() {
+            @Override
+            public ClusterClientAssignConfig convert(String source) {
+                List<ClusterGroupEntity> groupList = JSON.parseObject(source, new TypeReference<List<ClusterGroupEntity>>() {});
+                return groupList==null?null:extractClientAssignment(groupList);
+            }
+        });
+        ClusterClientConfigManager.registerServerAssignProperty(clientAssignDs.getProperty());
+    }
+
+
+    private void initStateProperty(String appName,final NacosConfig nacosConfig) {
+        // Cluster map format:
+        // [{"clientSet":["112.12.88.66@8729","112.12.88.67@8727"],"ip":"112.12.88.68","machineId":"112.12.88.68@8728","port":11111}]
+        // machineId: <ip@commandPort>, commandPort for port exposed to Sentinel dashboard (transport module)
+        String clusterMapDataId = appName + CLUSTER_MAP_DATA_ID_POSTFIX;
+        ReadableDataSource<String, Integer> clusterModeDs =  new CustomNacosDataSource<>(configService, properties, nacosConfig.getGroupId(),
+                clusterMapDataId, new Converter<String, Integer>() {
+            @Override
+            public Integer convert(String source) {
+                List<ClusterGroupEntity> groupList = JSON.parseObject(source, new TypeReference<List<ClusterGroupEntity>>() {});
+                if(groupList!=null){
+                    return extractMode(groupList);
+                }
+                return ClusterStateManager.CLUSTER_NOT_STARTED;
+            }
+        });
+        ClusterStateManager.registerProperty(clusterModeDs.getProperty());
+    }
+
+    private int extractMode(List<ClusterGroupEntity> groupList) {
+        // If any server group machineId matches current, then it's token server.
+        for(ClusterGroupEntity clusterGroupEntity:groupList){
+            if(this.machineEqual(clusterGroupEntity)){
+                return ClusterStateManager.CLUSTER_SERVER;
+            }
+        }
+
+        // If current machine belongs to any of the token server group, then it's token client.
+        // Otherwise it's unassigned, should be set to NOT_STARTED.
+        for(ClusterGroupEntity clusterGroupEntity:groupList){
+            if(clusterGroupEntity.getClientSet()!=null){
+               for(String str:clusterGroupEntity.getClientSet()){
+                   if(getCurrentMachineId().equals(str)){
+                       return ClusterStateManager.CLUSTER_CLIENT;
+                   }
+                }
+            }
+        }
+        return ClusterStateManager.CLUSTER_NOT_STARTED;
+    }
+
+
+    private ServerTransportConfig extractServerTransportConfig(List<ClusterGroupEntity> groupList) {
+        for(ClusterGroupEntity groupEntity:groupList){
+            if(this.machineEqual(groupEntity)){
+                return new ServerTransportConfig().setPort(groupEntity.getPort()).setIdleSeconds(600);
+            }
+        }
+        return null;
+    }
+
+    private ClusterClientAssignConfig extractClientAssignment(List<ClusterGroupEntity> groupList) {
+        for(ClusterGroupEntity clusterGroupEntity:groupList){
+            if(this.machineEqual(clusterGroupEntity)){
+                return null;
+            }
+        }
+        // Build client assign config from the client set of target server group.
+        for (ClusterGroupEntity group : groupList) {
+            if (group.getClientSet().contains(getCurrentMachineId())) {
+                String ip = group.getIp();
+                Integer port = group.getPort();
+                return new ClusterClientAssignConfig(ip, port);
+            }
+        }
+        return null;
+    }
+
+
+    private boolean machineEqual(/*@Valid*/ ClusterGroupEntity group) {
+        return getCurrentMachineId().equals(group.getMachineId());
+    }
+
+    private String getCurrentMachineId() {
+        // Note: this may not work well for container-based env.
+        return HostNameUtil.getIp() + SEPARATOR + TransportConfig.getRuntimePort();
+    }
+
+    private static final String SEPARATOR = "@";
+
+
     public NacosConfig getNacosConfig() {
         return nacosConfig;
     }
 
     public void setNacosConfig(NacosConfig nacosConfig) {
         this.nacosConfig = nacosConfig;
+    }
+
+    public NacosConfig getNacosClusterConfig() {
+        return nacosClusterConfig;
+    }
+
+    public void setNacosClusterConfig(NacosConfig nacosClusterConfig) {
+        this.nacosClusterConfig = nacosClusterConfig;
     }
 }
--- sentinel-yj/sentinel-yj-nacos/src/main/java/com/alibaba/csp/sentinel/yj/nacos/NacosInit.java
+++ sentinel-yj/sentinel-yj-nacos/src/main/java/com/alibaba/csp/sentinel/yj/nacos/NacosInit.java
@@ -4,14 +4,19 @@ import com.alibaba.csp.sentinel.config.SentinelConfig;
 import com.alibaba.csp.sentinel.init.InitFunc;
 import com.alibaba.csp.sentinel.init.InitOrder;
 import com.alibaba.csp.sentinel.util.StringUtil;
+import com.alibaba.fastjson.JSON;
 import com.taobao.diamond.manager.ManagerListener;
 import com.yunji.diamond.client.api.DiamondClient;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Properties;
 
+/**
+ *  nacos配置信息拉取并初始化
+ */
 @InitOrder(InitOrder.HIGHEST_PRECEDENCE)
 public class NacosInit implements InitFunc {
 
@@ -29,7 +34,9 @@ public class NacosInit implements InitFunc {
         diamondClient.setDataId(dataId);
         diamondClient.setPollingIntervalTime(10);
         diamondClient.setTimeout(2000L);
-        diamondClient.setManagerListener(managerListener);
+        if(diamondClient!=null){
+            diamondClient.setManagerListener(managerListener);
+        }
         /* 初始化diamond */
         diamondClient.init();
         return diamondClient;
@@ -37,50 +44,57 @@ public class NacosInit implements InitFunc {
 
     private void initNacos(){
 
+        /**
+         * 暂时不支持动态更新
+         */
         nacosDiamondClient = getDiamondClient("buriedpoint_nacos_config",null);
 
         Properties properties = new Properties();
 
         try (StringReader stringReader = new StringReader(nacosDiamondClient.getConfig())){
             properties.load(stringReader);
-            if(StringUtil.isNotBlank(properties.getProperty("csp.sentinel.dashboard.server"))){
-                SentinelConfig.setConfigIfAbsent("csp.sentinel.dashboard.server",properties.getProperty("csp.sentinel.dashboard.server"));
-            }
-
-            int heartbeat = NumberUtils.toInt(properties.getProperty("csp.sentinel.heartbeat.interval.ms"),-1);
-            if(heartbeat>0){
-                SentinelConfig.setConfig("csp.sentinel.heartbeat.interval.ms",properties.getProperty("csp.sentinel.heartbeat.interval.ms"));
-            }
-
-
-            NacosConfig nacosConfig = new NacosConfig();
-            nacosConfig.setGroupId(properties.getProperty("groupId"));
-            nacosConfig.setNamespaceId(properties.getProperty("namespaceId"));
-            nacosConfig.setRemoteAddress(properties.getProperty("remoteAddress"));
-
-
-
-            NacosDataSource nacosDataSource = new NacosDataSource();
-            nacosDataSource.setNacosConfig(nacosConfig);
-            nacosDataSource.start();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        if(StringUtil.isNotBlank(properties.getProperty("csp.sentinel.dashboard.server"))){
+            SentinelConfig.setConfigIfAbsent("csp.sentinel.dashboard.server",properties.getProperty("csp.sentinel.dashboard.server"));
+        }
+
+        int heartbeat = NumberUtils.toInt(properties.getProperty("csp.sentinel.heartbeat.interval.ms"),-1);
+        if(heartbeat>0){
+            SentinelConfig.setConfig("csp.sentinel.heartbeat.interval.ms",properties.getProperty("csp.sentinel.heartbeat.interval.ms"));
+        }
+
+
+        NacosConfig nacosConfig = new NacosConfig();
+        nacosConfig.setGroupId(properties.getProperty("groupId"));
+        nacosConfig.setNamespaceId(properties.getProperty("namespaceId"));
+        nacosConfig.setRemoteAddress(properties.getProperty("remoteAddress"));
+
+        //nacos集群配置
+        String clusterConfig = properties.getProperty("sentinel.cluster.config");
+        NacosConfig nacosConfigCluster = null;
+        if(StringUtils.isNoneBlank(clusterConfig)){
+            try{
+                nacosConfigCluster = JSON.parseObject(clusterConfig,NacosConfig.class);
+            }catch (Exception ex){
+
+            }
+
+        }
+
+
+
+
+        NacosDataSource nacosDataSource = new NacosDataSource();
+        nacosDataSource.setNacosConfig(nacosConfig);
+        nacosDataSource.setNacosClusterConfig(nacosConfigCluster);
+        nacosDataSource.start();
+
+
     }
 
-//    private void initSentinelConfig(){
-//
-//        sentinelConfigDiamondClient = getDiamondClient("buriedpoint_sentinel_config",null);
-//
-//
-//        Properties properties = new Properties();
-//        try {
-//            properties.load(new StringReader(sentinelConfigDiamondClient.getConfig()));
-//            properties.setProperty()
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
+
 }
